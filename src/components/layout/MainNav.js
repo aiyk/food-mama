ca8fff034919cd2029ab77930160902c2d86217d
aiyk <aiyk.ekwe@gmail.com>
2018-12-21 14:25:14 +0100

    main nav designs

--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -4,7 +4,21 @@ import './layout.scss';
 export default function MainNav() {
     return (
         <div className="main-nav">
+            <div className="logo-wrap">
+                <div className="logo-label1">food</div>
+                <div className="logo-label2">mama</div>
+            </div>
 
+            <div className="nav-links">
+                <div className="nav-link">Dashboard</div>
+                <div className="nav-link">User Management</div>
+                <div className="nav-link">Payments</div>
+                <div className="nav-link">Food Menu Manager</div>
+                <div className="nav-link">Dashboard</div>
+                <div className="nav-link">Dashboard</div>
+            </div>
+
+            <div className="nav-footer">food mama copyright &copy; {(new Date()).getFullYear()}</div>
         </div>
     )
 }
