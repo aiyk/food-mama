cf02634c750954440d94a73bff334bdad1d2878e
aiyk <aiyk.ekwe@gmail.com>
2018-12-21 13:16:18 +0100

    app layout

---
+++ src/components/layout/AppBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './layout.scss';
+
+export default function AppBar() {
+    return (
+        <div className="app-bar">
+
+        </div>
+    )
+}
---
+++ src/components/layout/MainNav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './layout.scss';
+
+export default function MainNav() {
+    return (
+        <div className="main-nav">
+
+        </div>
+    )
+}
