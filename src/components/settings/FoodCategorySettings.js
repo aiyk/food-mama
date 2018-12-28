eb7c44770be75cff02b617d1c24bc5d2f3519329
aiyk <aiyk.ekwe@gmail.com>
2018-12-28 22:57:55 +0100

    food categry setup

--- src/components/settings/FoodCategorySettings.js
+++ src/components/settings/FoodCategorySettings.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import Spinner from '../common/Spinner'
+import { getFoodCategories } from '../../store/actions/settingsActions'
+import Table from '../common/Table'
+import CreateFoodCategory from './CreateFoodCategory'
+import '../users/users.scss'
 
-export default class FoodCategorySettings extends Component {
+class FoodCategorySettings extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            openPopup: false
+        }
+    }
+
+    componentDidMount() {
+        this.props.getFoodCategories();
+    }
+    toggle_modal = () => {
+        this.setState({
+            openPopup: !this.state.openPopup
+        })
+    }
     render() {
+        const { foodCategory, loading } = this.props;
+
         return (
             <div>
-                Food Category Settings
-      </div>
+                <Table payload={foodCategory} loading={loading} toggle_modal={this.toggle_modal} />
+                {
+                    this.state.openPopup
+                        ? (<CreateFoodCategory toggle_modal={this.toggle_modal} />)
+                        : (null)
+                }
+
+            </div>
         )
     }
 }
+
+FoodCategorySettings.propTypes = {
+    getFoodCategories: PropTypes.func.isRequired,
+    foodCategory: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    foodCategory: state.settings.settings.foodCategory
+});
+
+export default connect(mapStateToProps, { getFoodCategories })(FoodCategorySettings)
